0ab2524a51042c472d99e62c26954eceecf51953
Samarth Narang <samanara@qti.qualcomm.com>
2026-01-16 12:23:30 -0800

    Adding licensing
    Signed-off-by: Samarth Narang <samanara@qti.qualcomm.com>

--- ci/setup_submodules.sh
+++ ci/setup_submodules.sh
@@ -1,3 +1,9 @@
+# 
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: BSD-3-Clause.
+# For more license information:
+#   https://github.com/qualcomm/hexagon-mlir/LICENSE.txt
+#
 #!/usr/bin/env bash
 set -euo pipefail
 
