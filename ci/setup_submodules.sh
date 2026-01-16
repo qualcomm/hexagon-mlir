6a3e26ebb02286fa6cb4b9d05edefea965fa2d67
Samarth Narang <samanara@qti.qualcomm.com>
2026-01-16 12:15:06 -0800

    Add submodules
    Add .gitmodules, in that add qcom specific patches
    
    Signed-off-by: Samarth Narang <samanara@qti.qualcomm.com>

--- ci/setup_submodules.sh
+++ ci/setup_submodules.sh
@@ -1,31 +1,26 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+REPO_ROOT="$(git rev-parse --show-toplevel)"
 echo "Configuring git for submodules"
-
+cd "${REPO_ROOT}"
 # Add submodules if missing
 if [ ! -d "triton" ]; then
-  git submodule add --force git@github.qualcomm.com:ai-tools/triton.git triton
+  git submodule add --force https://github.com/triton-lang/triton.git triton
+  cd triton
+  echo "Applying qcom specific patches to triton"
+  git checkout e44bd1c83c1c3e8deac7c4f02683cfb3cc395c8b
+  git apply "${REPO_ROOT}/third_party_software/patches/triton/third_party_triton.patch"
 fi
 
+cd "${REPO_ROOT}"
 if [ ! -d "triton_shared" ]; then
   git submodule add --force https://github.com/microsoft/triton-shared triton_shared
+  cd triton_shared
+  git checkout 2b728ad97bc02af821a0805b09075838911d4c19
+  echo "Applying qcom specific patches to triton_shared"
+  git apply "${REPO_ROOT}/third_party_software/patches/triton_shared/max_with_nan_propagation.patch"
+  git apply "${REPO_ROOT}/third_party_software/patches/triton_shared/tt_shared_split_dim.patch"
 fi
 
-# Initialize and fetch
-git submodule update --init --recursive
-
-# Checkout specific commits
-(
-  cd triton
-  git fetch --all
-  git checkout 66daa234f28cb316704688130401a7ec6cbdda78
-)
-
-(
-  cd triton_shared
-  git fetch --all
-  git checkout 2b728ad97bc02af821a0805b09075838911d4c19
-)
-
-echo "Submodules triton and triton_shared initialized and pinned."
+echo "Submodules triton and triton_shared initialized and patched successfully."
